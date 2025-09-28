a19a6cea25fdda2cb678f73ba3ca31081b7b26c1
Udu Jonaka Nnanna <udunnanna0@gmail.com>
2025-09-29 00:16:40 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve static files from "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/about', function(req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+
+app.get('/contact', function(req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+});
+
+app.listen(PORT, function() {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
